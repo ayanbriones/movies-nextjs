f8cbdf9f55f4bd762cbbb7e6a147000c0d6bf81b
Bryanth Briones <ayanbriones@gmail.com>
2023-07-31 20:41:39 +0300

    fix mobile layout

--- app/movie/[id]/layout.tsx
+++ app/movie/[id]/layout.tsx
@@ -29,21 +29,22 @@ export default async function MovieId({
   const data: Movie = await getData(params.id)
   return (
     <div className='min-h-screen p-10'>
-      <div className='h-[80vh] relative mb-4'>
+      <div className='lg:h-[80vh] relative min-w-min overflow-hidden mb-4'>
         <Image
           src={`https://image.tmdb.org/t/p/original/${data.backdrop_path}`}
           alt='image movie'
-          className='object-cover w-full rounded-lg'
-          fill
+          className='w-full object-contain rounded-lg'
+          width={1920}
+          height={1080}
         />
       </div>
       <div className='flex flex-col lg:flex-row mt-10 gap-x-2'>
-        <div className='flex flex-row gap-x-5'>
+        <div className='flex flex-col md:flex-row gap-x-5'>
           <div className='block min-w-max'>
             <Image
               src={`https://image.tmdb.org/t/p/w500/${data.poster_path}`}
               alt=''
-              width={250}
+              width={150}
               height={500}
               className='rounded-lg'
             />
@@ -63,7 +64,7 @@ export default async function MovieId({
             <p className='my-4'>{data.overview}</p>
           </div>
         </div>
-        <div className='w-full my-8'>{children}</div>
+        <div className='w-full my-8 lg:my-0'>{children}</div>
       </div>
     </div>
   )
